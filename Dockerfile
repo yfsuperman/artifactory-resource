f77f20cbcf0ae833764818ad7508991caca1d96d
Luciano Silva <lsilva@pivotal.io>
2016-11-15 23:07:29 -0500

    Refactor Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,10 @@ FROM concourse/buildroot:curl
 
 ADD assets/ /opt/resource/
 ADD test/ /opt/resource-tests/
+ADD tools/ /opt/tools/
+
+RUN rm /usr/bin/jq
+RUN mv /opt/tools/jq /usr/bin/jq
 
 # Run tests
 # RUN /opt/resource-tests/test-check.sh
--- test/test-check.sh
+++ test/test-check.sh
@@ -6,51 +6,57 @@ source $(dirname $0)/helpers.sh
 
 it_can_list_releases_from_artifactory() {
 
-  local local_ip=$(find_docker_host_ip)	
+  # local local_ip=$(find_docker_host_ip)
   #local_ip="localhost"
+  artifactory_ip=$ART_IP
+  TMPDIR=/tmp
 
   local src=$(mktemp -d $TMPDIR/in-src.XXXXXX)
-  local endpoint="http://${local_ip}:8081/artifactory"
-  local regex="carshare-(admin|api|customer)-(?<version>.*).tar.gz"
-  local folder="/UrbanActive/Products/Maven/admin"
+  local endpoint="http://${artifactory_ip}:8081/artifactory"
+  local regex="ecd-front-(?<version>.*).tar.gz"
+  local folder="/generic/ecd-front"
 
-  check_without_credentials_and_version $endpoint $regex $folder $src 
-  
+  check_without_credentials_and_version $endpoint $regex $folder $src
+
 }
 
 it_can_list_releases_from_artifactory_with_version() {
 
-  local local_ip=$(find_docker_host_ip) 
+  # local local_ip=$(find_docker_host_ip)
   #local_ip="localhost"
+  artifactory_ip=$ART_IP
+  TMPDIR=/tmp
 
   local src=$(mktemp -d $TMPDIR/in-src.XXXXXX)
-  local endpoint="http://${local_ip}:8081/artifactory"
-  local regex="carshare-(admin|api|customer)-(?<version>.*).tar.gz"
-  local folder="/UrbanActive/Products/Maven/admin"
-  local version="1.0.0"
+  local endpoint="http://${artifactory_ip}:8081/artifactory"
+  local regex="ecd-front-(?<version>.*).tar.gz"
+  local folder="/generic/ecd-front"
+  local version="20161109222826"
 
   check_without_credentials_with_version $endpoint $regex $folder $version $src
-  
+
 }
 
 it_can_list_releases_from_protected_artifactory_with_version() {
 
-  local local_ip=$(find_docker_host_ip) 
+  # local local_ip=$(find_docker_host_ip)
   #local_ip="localhost"
+  artifactory_ip=$ART_IP
+  TMPDIR=/tmp
+
 
   local src=$(mktemp -d $TMPDIR/in-src.XXXXXX)
-  local endpoint="http://${local_ip}:8081/artifactory"
-  local regex="carshare-(admin|api|customer)-(?<version>.*).tar.gz"
-  local folder="/UrbanActive/Products/Maven/admin"
-  local version="1.0.0"
-  local username="admin"
-  local password="password"
+  local endpoint="http://${artifactory_ip}:8081/artifactory"
+  local regex="ecd-front-(?<version>.*).tar.gz"
+  local folder="/generic/ecd-front"
+  local version="20161109222826"
+  local username="${ART_USER}"
+  local password="${ART_PWD}"
 
   check_with_credentials_with_version $endpoint $regex $username $password $folder $version $src
-  
+
 }
 
 run it_can_list_releases_from_artifactory
 run it_can_list_releases_from_artifactory_with_version
 run it_can_list_releases_from_protected_artifactory_with_version
-
